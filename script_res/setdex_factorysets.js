cc924d56ecd28a7f9902c71d3d0aa5864b0acf0a
LegoFigure11 <legofigurefb@gmail.com>
2017-05-28 02:36:22 +1000

    Add Randbats sets Aegislash -> Beedrill
    am lazy rn and will make a parser so this can all be done in one hit

--- script_res/setdex_factorysets.js
+++ script_res/setdex_factorysets.js
@@ -1,2 +1,340 @@
 var SETDEX_FACTORYSETS={
+	  "Aegislash" { 
+    "Weakness Policy": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 4
+        "sa": 252
+      }, 
+      "ivs": { 
+        "se": 0
+      }, 
+      "nature": "Quiet", 
+      "ability": "Stance Change", 
+      "item": "Weakness Policy", 
+      "moves": [ 
+        "Flashh Cannon", 
+        "Shadow Ball", 
+        "Sacred Sword", 
+        "Shadow Sneak" 
+      ] 
+    },
+    "Special Ghostium Z": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 4
+        "sa": 252
+      }, 
+      "ivs": { 
+        "se": 0
+      }, 
+      "nature": "Quiet", 
+      "ability": "Stance Change", 
+      "item": "Ghostium Z", 
+      "moves": [ 
+        "Flash Cannon", 
+        "Shadow Ball", 
+        "Sacred Sword", 
+        "Shadow Sneak" 
+      ] 
+    },
+    "Physical Ghostium Z": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 252
+        "sd": 4
+      }, 
+      "nature": "Brave", 
+      "ability": "Stance Change", 
+      "item": "Ghostium Z", 
+      "moves": [ 
+        "King's Shield", 
+        "Shadow Claw", 
+        "Sacred Sword", 
+        "Shadow Sneak" 
+      ] 
+    },
+    "Fast Physical LO": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 4
+        "at": 252
+        "se": 252
+      }, 
+      "nature": "Jolly", 
+      "ability": "Stance Change", 
+      "item": "Life Orb", 
+      "moves": [ 
+        "Iron Head", 
+        "Shadow Claw", 
+        "Sacred Sword", 
+        "Shadow Sneak" 
+      ] 
+    },
+    "SubToxic": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 204
+        "sa": 52
+        "se": 252
+      }, 
+      "ivs": { 
+        "at": 0
+      }, 
+      "nature": "Timid", 
+      "ability": "Stance Change", 
+      "item": "Leftovers", 
+      "moves": [ 
+        "Hidden Power Ice", 
+        "Hidden Power Fire", 
+        "Substitute", 
+        "Shadow Ball" 
+      ] 
+    },
+    "Focus Sash": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 4
+        "sa": 252
+        "se": 252
+      }, 
+      "nature": "RNature", 
+      "ability": "Stance Change", 
+      "item": "Focus Sash", 
+      "moves": [ 
+        "Shadow Ball", 
+        "Flash Cannon", 
+        "Hidden Power Ice / Head Smash", 
+        "Shadow Sneak" 
+      ] 
+    } 
+  },
+    "Araquanid" { 
+    "Waterium Z": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 252
+        "sd": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Water Bubble", 
+      "item": "Waterium Z", 
+      "moves": [ 
+        "Liquidation", 
+        "Leech Life", 
+        "Mirror Coat", 
+        "Poison Jab" 
+      ] 
+    },
+    "Assault Vest": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 252
+        "sd": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Water Bubble", 
+      "item": "Assault Vest", 
+      "moves": [ 
+        "Liquidation", 
+        "Leech Life", 
+        "Mirror Coat", 
+        "Poison Jab" 
+      ] 
+    },
+    "Choice Band": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 252
+        "sd": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Water Bubble", 
+      "item": "Choice Band", 
+      "moves": [ 
+        "Liquidation", 
+        "Leech Life", 
+        "Crunch", 
+        "Poison Jab" 
+      ] 
+    },
+    "Assault Vest": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "at": 252
+        "sd": 4
+      }, 
+      "nature": "Quiet", 
+      "ability": "Water Bubble", 
+      "item": "Assault Vest", 
+      "moves": [ 
+        "Scald", 
+        "Leech Life", 
+        "Mirror Coat", 
+        "Ice Beam" 
+      ] 
+    },
+    "Choice Specs": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "df": 4
+        "sa": 252
+      }, 
+      "ivs": { 
+        "at": 0
+      }, 
+      "nature": "Modest", 
+      "ability": "Water Bubble", 
+      "item": "Choice Specs", 
+      "moves": [ 
+        "Surf", 
+        "Ice Beam", 
+        "Hidden Power Fire"
+      ] 
+    } 
+  },
+    "Arcanine" { 
+    "Firium Z": { 
+      "level": 50, 
+      "evs": { 
+        "at": 252
+        "sd": 4
+        "se": 252
+      }, 
+      "nature": "Adamant", 
+      "ability": "Intimidate", 
+      "item": "Firium Z", 
+      "moves": [ 
+        "Flare Blitz", 
+        "Extreme Speed", 
+        "Close Combat", 
+        "Wild Charge" 
+      ] 
+    },
+    "Defensive": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 252
+        "df": 252
+        "sa": 4
+      }, 
+      "ivs": { 
+        "at": 0
+      }, 
+      "nature": "Bold", 
+      "ability": "Intimidate", 
+      "item": "Rocky Helmet", 
+      "moves": [ 
+        "Will-O-Wisp", 
+        "Morning Sun", 
+        "Flamethrower", 
+        "Snarl" 
+      ] 
+    } 
+  },
+    "Azumarill" { 
+    "Belly Drum": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 228
+        "at": 252
+        "df": 12
+        "sd": 12
+        "se": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Huge Power", 
+      "item": "Sitrus Berry", 
+      "moves": [ 
+        "Superpower", 
+        "Aqua Jet", 
+        "Play Rough", 
+        "Waterfall" 
+      ] 
+    },
+    "Z Belly Drum": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 228
+        "at": 252
+        "df": 12
+        "sd": 12
+        "se": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Huge Power", 
+      "item": "Normalium Z", 
+      "moves": [ 
+        "Superpower", 
+        "Aqua Jet", 
+        "Play Rough", 
+        "Waterfall" 
+      ] 
+    },
+    "Choice Band": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 228
+        "at": 252
+        "df": 12
+        "sd": 12
+        "se": 4
+      }, 
+      "nature": "Adamant", 
+      "ability": "Huge Power", 
+      "item": "Choice Band", 
+      "moves": [ 
+        "Aqua Jet", 
+        "Play Rough", 
+        "Waterfall", 
+        "Superpower" 
+      ] 
+    },
+    "set name": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 124
+        "at": 252
+        "df": 12
+        "sd": 28
+        "se": 92
+      }, 
+      "nature": "Adamant", 
+      "ability": "Huge Power", 
+      "item": "Assault Vest", 
+      "moves": [ 
+        "Aqua Jet", 
+        "Play Rough", 
+        "Waterfall", 
+        "Superpower" 
+      ] 
+    } 
+  },
+    "Beedrill" { 
+    "Mega": { 
+      "level": 50, 
+      "evs": { 
+        "hp": 4
+        "at": 252
+        "se": 252
+      }, 
+      "nature": "Jolly", 
+      "ability": "Swarm", 
+      "item": "Beedrillite", 
+      "moves": [ 
+        "U-turn", 
+        "Poison Jab", 
+        "Drill Run", 
+        "Knock Off" 
+      ] 
+    } 
+  },
 };
