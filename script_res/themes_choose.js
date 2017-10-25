d814178714f35c024386fb90f0b61536d60fe6c6
LegoFigure11 <legofigurefb@gmail.com>
2017-10-25 18:34:05 +1100

    Add Themes, and do some other stuff (#16)
    * Test new css
    
    * Add Theme Button + Fuctionality
    
    Also move some things
    
    * Change Header Border and InputBox Border width
    
    * Attempt to add Cookies to remember theme selection
    
    * Ammept to use cookies again
    
    * Add Themes, reorganise files, remove NHB
    
    * Change directory for image sources
    
    * Fix typo in loading of scripts

---
+++ script_res/themes_choose.js
@@ -0,0 +1,48 @@
+var changetheme = function()
+{
+	
+	
+	if (document.getElementById("theme").value === "aegislash") {
+		loadjscssfile("script_res/themes/ap_calc.css", "css")
+		loadjscssfile("script_res/themes/nb_calc.css", "css")
+		loadjscssfile("script_res/themes/select2.css", "css")
+		console.log("loaded aegislash theme")
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
+		removejscssfile("script_res/themes/dark_ap_calc.css", "css")
+		removejscssfile("script_res/themes/dark_nb_calc.css", "css")
+		//removejscssfile("script_res/themes/dark_select2.css", "css")
+		/*removejscssfile("script_res/themes/legacy_ap_calc.css", "css")
+		removejscssfile("script_res/themes/legacy_nb_calc.css", "css")
+		removejscssfile("script_res/themes/legacy_select2.css", "css")*/
+	} else if (document.getElementById("theme").value === "dark") {
+		loadjscssfile("script_res/themes/dark_ap_calc.css", "css")
+		loadjscssfile("script_res/themes/dark_nb_calc.css", "css")
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
+		//loadjscssfile("script_res/dark_select2.css", "css")
+		/*removejscssfile("script_res/themes/ap_calc.css", "css")
+		removejscssfile("script_res/themes/nb_calc.css", "css")
+		removejscssfile("script_res/themes/select2.css", "css")
+		removejscssfile("script_res/themes/legacy_ap_calc.css", "css")
+		removejscssfile("script_res/themes/legacy_nb_calc.css", "css")
+		removejscssfile("script_res/themes/legacy_select2.css", "css")*/
+		console.log("loaded dark theme")
+	} else if (document.getElementById("theme").value === "legacy") {
+		loadjscssfile("script_res/themes/legacy_ap_calc.css", "css")
+		loadjscssfile("script_res/themes/legacy_nb_calc.css", "css")
+		loadjscssfile("script_res/themes/legacy_select2.css", "css")
+		removejscssfile("script_res/themes/dark_ap_calc.css", "css")
+		removejscssfile("script_res/themes/dark_nb_calc.css", "css")
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
+		//removejscssfile("script_res/themes/dark_select2.css", "css")
+		/*removejscssfile("script_res/themes/ap_calc.css", "css")
+		removejscssfile("script_res/themes/nb_calc.css", "css")
+		removejscssfile("script_res/themes/select2.css", "css")*/
+		console.log("loaded legacy theme")
+	}
+}
