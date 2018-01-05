ff926af968cd1b976900ead612ab8eb26f60ac7a
cant say <jake.c.king@live.com.au>
2018-01-06 05:30:49 +1100

    recoil fix 3

--- script_res/ap_calc.js
+++ script_res/ap_calc.js
@@ -559,7 +559,7 @@ function calculate() {
 		  Math.floor(minDamage * p1.moves[i].recoilPercentage * 48 / p1.maxHP);
 		var maxRecoilDamage = "%" === '%' ? Math.floor(maxDamage * p1.moves[i].recoilPercentage * 1000 / p1.maxHP) / 10 :
 		  Math.floor(minDamage * p1.moves[i].recoilPercentage * 48 / p1.maxHP);
-		if (if (minDamage > p2.maxHP) {
+		if (maxDamage > p2.maxHP) {
 		  minRecoilDamage = Math.floor(p2.maxHP * p1.moves[i].recoilPercentage * 1000 / p1.maxHP) / 10;
 		  maxRecoilDamage = Math.floor(p2.maxHP * p1.moves[i].recoilPercentage * 1000 / p1.maxHP) / 10;
 		} else if ("%" !== '%' && minRecoilDamage > 48) {
@@ -607,7 +607,7 @@ function calculate() {
 		  Math.floor(minDamage * p2.moves[i].recoilPercentage * 48 / p2.maxHP);
 		var maxRecoilDamage = "%" === '%' ? Math.floor(maxDamage * p2.moves[i].recoilPercentage * 1000 / p2.maxHP) / 10 :
 		  Math.floor(minDamage * p2.moves[i].recoilPercentage * 48 / p2.maxHP);
-		if (if (minDamage > p1.maxHP) {
+		if (maxDamage > p1.maxHP) {
 		  minRecoilDamage = Math.floor(p1.maxHP * p2.moves[i].recoilPercentage * 1000 / p2.maxHP) / 10;
 		  maxRecoilDamage = Math.floor(p1.maxHP * p2.moves[i].recoilPercentage * 1000 / p2.maxHP) / 10;
 		} else if ("%" !== '%' && minRecoilDamage > 48) {
