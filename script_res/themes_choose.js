feae1dfa119d1e542dbf48f6ac198ac9764063b3
LegoFigure11 <legofigurefb@gmail.com>
2018-01-05 22:51:20 +1100

    Change Centre Icon when changing themes

--- script_res/themes_choose.js
+++ script_res/themes_choose.js
@@ -7,11 +7,12 @@ var changetheme = function()
 		loadjscssfile("script_res/themes/nb_calc.css", "css")
 		loadjscssfile("script_res/themes/select2.css", "css")
 		console.log("loaded aegislash theme")
-		//var select = document.getElementById("theme");
-		//var selectedItem = select.value;
-		//createCookie("selectedItem",selectedItem)
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
 		removejscssfile("script_res/themes/dark_ap_calc.css", "css")
 		removejscssfile("script_res/themes/dark_nb_calc.css", "css")
+		document.getElementById("midimg").src="script_res/image_res/aegi trozei.png"
 		//removejscssfile("script_res/themes/dark_select2.css", "css")
 		/*removejscssfile("script_res/themes/legacy_ap_calc.css", "css")
 		removejscssfile("script_res/themes/legacy_nb_calc.css", "css")
@@ -19,9 +20,10 @@ var changetheme = function()
 	} else if (document.getElementById("theme").value === "dark") {
 		loadjscssfile("script_res/themes/dark_ap_calc.css", "css")
 		loadjscssfile("script_res/themes/dark_nb_calc.css", "css")
-		//var select = document.getElementById("theme");
-		//var selectedItem = select.value;
-		//createCookie("selectedItem",selectedItem)
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
+		document.getElementById("midimg").src="script_res/image_res/cantsay.png"
 		//loadjscssfile("script_res/dark_select2.css", "css")
 		/*removejscssfile("script_res/themes/ap_calc.css", "css")
 		removejscssfile("script_res/themes/nb_calc.css", "css")
@@ -36,9 +38,10 @@ var changetheme = function()
 		loadjscssfile("script_res/themes/legacy_select2.css", "css")
 		removejscssfile("script_res/themes/dark_ap_calc.css", "css")
 		removejscssfile("script_res/themes/dark_nb_calc.css", "css")
-		//var select = document.getElementById("theme");
-		//var selectedItem = select.value;
-		//createCookie("selectedItem",selectedItem)
+		document.getElementById("midimg").src="script_res/image_res/cantsay.png"
+		var select = document.getElementById("theme");
+		var selectedItem = select.value;
+		createCookie("selectedItem",selectedItem)
 		//removejscssfile("script_res/themes/dark_select2.css", "css")
 		/*removejscssfile("script_res/themes/ap_calc.css", "css")
 		removejscssfile("script_res/themes/nb_calc.css", "css")
