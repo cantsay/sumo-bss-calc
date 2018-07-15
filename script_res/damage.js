11062e131e3f63b7b03aaeb3e5e0913f3e4b5d05
cant say <jake.c.king@live.com.au>
2018-07-15 22:59:29 +1000

    removed duplicate line regarding thousand arrows

--- script_res/damage.js
+++ script_res/damage.js
@@ -126,7 +126,6 @@ function getDamageResult(attacker, defender, move, field) {
             move.type === "Fire" && defAbility.indexOf("Flash Fire") !== -1 ||
             move.type === "Water" && ["Dry Skin", "Storm Drain", "Water Absorb"].indexOf(defAbility) !== -1 ||
             move.type === "Electric" && ["Lightning Rod", "Lightningrod", "Motor Drive", "Volt Absorb"].indexOf(defAbility) !== -1 ||
-            move.type === "Ground" && !field.isGravity && defAbility === "Levitate" ||
             move.type === "Ground" && !field.isGravity && move.name !== "Thousand Arrows" && defAbility === "Levitate" ||
             move.isBullet && defAbility === "Bulletproof" ||
             move.isSound && defAbility === "Soundproof") {
